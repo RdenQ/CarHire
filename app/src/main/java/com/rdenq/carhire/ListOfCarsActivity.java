880e73f2ea1468648e3fb19401e901b6262432a3
Izabela Pogorzelska <izabela_p@ie.ibm.com>
2018-12-08 20:05:30 +0000

    Initial commit

---
+++ app/src/main/java/com/rdenq/carhire/ListOfCarsActivity.java
@@ -0,0 +1,13 @@
+package com.rdenq.carhire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListOfCarsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_cars);
+    }
+}
